aa120d05206613be9c6d82b57dab3f008b2c653b
Taran Zorn <21.taran.zorn@ksd.org>
2020-02-12 08:02:13 -0800

    Random Planets

---
+++ GroupMoblie/Assets/Scripts/Planets/PlanetCheck.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetCheck : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("foo");
+        if (collision.gameObject.tag == "green" || collision.gameObject.tag == "purple" || collision.gameObject.tag == "red")
+        {
+            Debug.Log("do the thoign");
+            Destroy(gameObject);
+            Randomish.counterA -= 1;
+        }
+    }
+}
--- GroupMoblie/Assets/Scripts/Planets/Randomish.cs
+++ GroupMoblie/Assets/Scripts/Planets/Randomish.cs
@@ -8,13 +8,18 @@ public class Randomish : MonoBehaviour
     public GameObject purplpe;
     public GameObject red;
     public Transform blue;
+
     float x;
     float y;
+    GameObject newobject;
+    public int maxScale;
+    public int minScale;
+    float scale;
 
     public float maxSpawnDistA = 5;
     float NmaxSpawnDistA;
     public float maxPlanetsA = 4;
-    int counterA= 0;
+    public static int counterA= 0;
 
     // Start is called before the first frame update
     void Start()
@@ -22,26 +27,37 @@ public class Randomish : MonoBehaviour
         NmaxSpawnDistA = 0 - (maxSpawnDistA * 2);
         while(counterA <= maxPlanetsA)
         {
-        int Rplanet = Random.Range(1, 3);
+        int Rplanet = Random.Range(1, 4);
+            float x = 0;
+            float y = 0;
+            float scale = 0;
+            newobject = null;
+
         switch (Rplanet)
         {
                case 1:
                     //  Debug.Log("Hello");
                     x = Random.Range(NmaxSpawnDistA, maxSpawnDistA);                   
                     y = Random.Range(NmaxSpawnDistA, maxSpawnDistA);
-                    Instantiate(green, new Vector3(x, y, blue.position.z), Quaternion.identity);
+                    scale = Random.Range(minScale, maxScale);
+                     newobject = Instantiate(green, new Vector3(x, y, blue.position.z), Quaternion.identity);
+                    newobject.transform.localScale = new Vector3(scale, scale, 1);
                     counterA += 1;
                     break;
                 case 2:
                      x = Random.Range(NmaxSpawnDistA, maxSpawnDistA);
                      y = Random.Range(NmaxSpawnDistA, maxSpawnDistA);
-                    Instantiate(purplpe, new Vector3(x, y, blue.position.z), Quaternion.identity);
+                    scale = Random.Range(minScale, maxScale);
+                    newobject = Instantiate(purplpe, new Vector3(x, y, blue.position.z), Quaternion.identity);
+                    newobject.transform.localScale = new Vector3(scale, scale, 1);
                     counterA += 1;
                     break;
                 case 3:
                     x = Random.Range(NmaxSpawnDistA, maxSpawnDistA);
                     y = Random.Range(NmaxSpawnDistA, maxSpawnDistA);
-                    Instantiate(red, new Vector3(x, y, blue.position.z), Quaternion.identity);
+                    scale = Random.Range(minScale, maxScale);
+                    newobject = Instantiate(red, new Vector3(x, y, blue.position.z), Quaternion.identity);
+                    newobject.transform.localScale = new Vector3(scale, scale, 1);
                     counterA += 1;
                     break;
 
@@ -49,11 +65,5 @@ public class Randomish : MonoBehaviour
         } 
 
             
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
