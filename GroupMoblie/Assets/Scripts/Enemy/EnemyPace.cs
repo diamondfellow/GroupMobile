a13073095f3aa3d4244c14c0907248f42db0cd47
Max Van Den Dikkenberg <21.Max.Van@ksd.org>
2020-02-27 09:27:49 -0800

    Credits

---
+++ GroupMoblie/Assets/Scripts/Enemy/EnemyPace.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPace : MonoBehaviour
+{
+    float timer = 0;
+    public Vector2 moveDir;
+    public float moveSpeed = 3.0f;
+    public float paceDuration = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveDir.Normalize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= paceDuration)
+        {
+            //do something
+            moveDir *= -1;
+            timer = 0;
+        }
+        GetComponent<Rigidbody2D>().velocity = moveDir * moveSpeed;
+    }
+}
