566f8597451ccd46ca371eef6630ba393e3aa348
Luke Roby <20.luke.roby@ksd.org>
2020-02-28 08:31:00 -0800

    ambler

---
+++ GroupMoblie/Assets/Scripts/Timer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public float startingTimer = 10.0f;
+    public Text TimeToString;
+    public static bool gameStart = false;
+    bool keepTiming;
+    float timer;
+
+  
+
+   
+    void Start()
+    {
+        startingTimer -= Time.deltaTime;
+    }
+
+    
+
+    void Update()
+    {
+        if (gameStart = true)
+        {
+            
+        }
+
+        if (keepTiming)
+        {
+            UpdateTime();
+        }
+    }
+
+    void UpdateTime()
+    {
+        timer = Time.time - startingTimer;
+        
+    }
+
+    float StopTimer()
+    {
+        keepTiming = false;
+        return timer;
+    }
+}
